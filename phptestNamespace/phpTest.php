d080d33181995f4cdee3143eead9fecfd8a14a2c
gtashnik <phpshnov@yandex.ru>
2020-03-07 09:54:43 +0300

    some changes

--- phptestNamespace/phpTest.php
+++ phptestNamespace/phpTest.php
@@ -73,45 +73,9 @@ if ($result->num_rows > 0) {
 $conn->close(); 
 
     }
+  
+theBase();
     
 } // конец класса database
 
-
-
-
-
-
-
-class randomQuotes {
-    
-    public function getRandomQuotes() {
-        
-        // get quotes
-        
-    $quotes = file_get_contents( __DIR__ . '/../inc/quotes.json');
-    
-    // decoding quotes 
-        
-     $quotes = json_decode($quotes, true);    
-    
-    // get a random index number 
-        
-    $index = mt_rand( 0, count( $quotes ) );
-        
-    // return the random quote
-    return $quotes[ $index ];
-    
-        
-    }
-    
-    public function generate() {
-    
-    return $this->getRandomQuotes();
-    
-}
-    
-}
-
-
-    
     
